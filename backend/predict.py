f004348ec4c291ac0792cc87fe835c6e0aa7de51
alfredd25 <alfredbiju7@gmail.com>
2025-04-21 11:22:14 +0530

    Fix CORS to allow Chrome extension

--- backend/predict.py
+++ backend/predict.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
 model_dir = os.path.join(os.path.dirname(__file__), "model")
 classifier = joblib.load(os.path.join(model_dir, "classifier.pkl"))
